5db1b36601490e685dd3f555652f23cdc9a067ce
yuntao <yuntao@starmerx.com>
2019-12-25 17:22:28 +0800

    m

--- machine_learn/othertest/timeconverter.py
+++ machine_learn/othertest/timeconverter.py
@@ -3,3 +3,7 @@ import pytz
 a = datetime.now(pytz.UTC) - timedelta(days=14)
 print a
 print a.strftime('%F %T')
+#order_dict['paid_time'] = datetime.strptime(order_dict['paid_at'], '%a, %d %b %Y %H:%M:%S GMT').strftime('%Y-%m-%d %H:%M:%S')
+import dateutil
+order_dict['paid_time'] = dateutil.parser.parse(order_dict['paid_at'])
+### https://stackoverflow.com/questions/38189867/valueerror-time-data-fri-mar-11-155957-est-2016-does-not-match-format-a-b
