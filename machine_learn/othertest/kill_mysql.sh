7b11afa1cd98aeb512cbb8d50053fa60c031dd3a
yuntao <yuntao@starmerx.com>
2020-01-08 18:29:21 +0800

    m

--- machine_learn/othertest/kill_mysql.sh
+++ machine_learn/othertest/kill_mysql.sh
@@ -1,6 +1,6 @@
 #crontab -e
 #/usr/sbin/crond start
-aaa=`mysql -h 172.18.40.34 -u starpro -p123456 -P 3306 -e 'show processlist' | awk '{if($6>10&&$5=="Sleep"||$5=="Query") print $1}'`
+aaa=`mysql -h 172.18.40.34 -u starpro -p123456 -P 3306 -e 'show processlist' | awk '{if($6>3&&$5=="Sleep"||$5=="Query") print $1}'`
 for i in $aaa
 do
 	mysql -h 172.18.40.34 -u starpro -p123456 -P 3306 -e 'kill '$i
