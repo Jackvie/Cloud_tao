87b2e8419028eecfb250108d0e7d8db082fd73a0
yuntao <yuntao@starmerx.com>
2020-02-08 18:11:07 +0800

    m

---
+++ mydrf/firstdemo/sh/__init__.py
@@ -0,0 +1,6 @@
+# sudo apt install rar unrar
+# unrar x ???????.rar
+# rar zip linux 网址
+# https://www.cnblogs.com/qhorse/p/7929883.html
+# jquery 插件网址
+# https://www.html5tricks.com/category/jquery-plugin/
